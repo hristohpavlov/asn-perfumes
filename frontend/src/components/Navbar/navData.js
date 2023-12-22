02d868894dd4cf28afa41e87d4bbddc8e5713034
hristohpavlov <hristohpavlov@gmail.com>
2023-12-22 18:28:38 +0200

    Init perfume website

---
+++ frontend/src/components/Navbar/navData.js
@@ -0,0 +1,42 @@
+export const navLinks = [
+  // {
+  //   label: 'Locations',
+  //   pathname: '/sites',
+  // },
+  {
+    label: 'Products',
+    pathname: '/products',
+  },
+  // {
+  //   label: 'Resume',
+  //   pathname: '/resume',
+  // },
+  {
+    label: 'About',
+    pathname: '/uses',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Instagram',
+    url: 'https://instagram.com',
+    icon: 'instagram',
+  },
+  // {
+  //   label: 'Facebook',
+  //   url: 'https://www.facebook.com',
+  //   icon: 'facebook',
+  // },
+  // {
+  //   label: 'TikTok',
+  //   url: 'https://tiktok.com',
+  //   icon: 'tiktok',
+  // },
+  
+  
+];
